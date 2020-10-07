21c2a59c625350e2efc5b9513b016c1232176a51
Hamish Willee <hamishwillee@gmail.com>
2020-10-06 22:54:18 -0700

    Add google analytics

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -15,8 +15,13 @@ module.exports = {
         locales: true,
       }],
       '@vuepress/back-to-top',
-      'vuepress-plugin-right-anchor', //This is actually "right side page TOC"
-      ],
+      'vuepress-plugin-right-anchor', //This is the "right side page TOC",
+      ['@vuepress/google-analytics',
+        {
+        'ga': 'UA-33658859-3'
+        }
+      ]
+  ],
   locales: {
     // The key is the path for the locale to be nested under.
     // As a special case, the default locale can use '/' as its path.
