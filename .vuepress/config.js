c4428a34c11643ea9782b8deedad29c72c7dbdf1
Hamish Willee <hamishwillee@gmail.com>
2020-11-03 20:28:58 -0800

    Add korean to build by default

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -8,6 +8,7 @@ module.exports = {
   patterns : ['README.md',
       'en/**/*.md',
       'zh/**/*.md',
+      'ko/**/*.md',
       // 'de/**/*.md',
       // 'ja/**/*.md',
       // 'ru/**/*.md',
@@ -45,6 +46,11 @@ module.exports = {
       title: 'PX4 User Guide',
       description: 'PX4 is the Professional Autopilot. Developed by world-class developers from industry and academia, and supported by an active world wide community, it powers all kinds of vehicles from racing and cargo drones through to ground vehicles and submersibles.'
     },
+    '/ko/': {
+      lang: 'ko-KR', // this will be set as the lang attribute on <html>
+      title: 'PX4 오토파일럿 사용자 설명서',
+      description: 'PX4 is the Professional Autopilot. Developed by world-class developers from industry and academia, and supported by an active world wide community, it powers all kinds of vehicles from racing and cargo drones through to ground vehicles and submersibles.'
+    },
     '/zh/': {
       lang: 'zh-CN',
       title: 'PX4 自动驾驶用户指南',
@@ -78,6 +84,7 @@ module.exports = {
     editLinkText: 'Help us improve this page!',
 
     locales: {
+      //English
       '/en/': {
         // text for the language dropdown
         selectText: 'Languages',
@@ -135,10 +142,68 @@ module.exports = {
         }
       },
       
+      //Korean
+      '/ko/': {
+        // text for the language dropdown
+        selectText: 'Languages',
+        // label for this locale in the language dropdown
+        label: '한국어 (Korean)',
+        // Aria Label for locale in the dropdown
+        ariaLabel: 'Languages',
+        // text for the edit-on-github link
+        editLinkText: 'Edit this page on GitHub',
+        // config for Service Worker
+        serviceWorker: {
+          updatePopup: {
+            message: "New content is available.",
+            buttonText: "Refresh"
+          }
+        },
+        // algolia docsearch options for current locale
+        algolia: {},
+        nav: [
+          { text: 'PX4', link: 'http://px4.io/' , ariaLabel: 'PX4 Site' },
+          { text: 'QGroundControl', link: 'http://qgroundcontrol.com/' , ariaLabel: 'QGC' },
+          { text: 'MAVSDK', link: 'https://www.dronecode.org/sdk/' , ariaLabel: 'MAVSDK' },
+          { text: 'MAVLINK', link: 'https://mavlink.io/en/' , ariaLabel: 'MAVLINK site' },
+          {
+            text: 'Docs',
+            ariaLabel: 'Documentation Menu',
+            items: [
+              { text: 'PX4 Developer Guide', link: 'https://dev.px4.io/master/en/' },
+              { text: 'QGroundControl User Guide', link: 'https://docs.qgroundcontrol.com/en/' },
+              { text: 'QGroundControl Developer Guide', link: 'https://dev.qgroundcontrol.com/en/' },
+              { text: 'MAVLink Guide', link: 'https://mavlink.io/en/' },
+              { text: 'MAVSDK', link: 'https://mavsdk.mavlink.io/' },
+              { text: 'Dronecode Camera Manager', link: 'https://camera-manager.dronecode.org/en/' },    
+              
+            ]
+          },
       
+          { text: 'Support', link: 'http://discuss.px4.io/' , ariaLabel: 'Support' },
+          {
+            text: 'Version',
+            ariaLabel: 'Versions Menu',
+            items: [
+              { text: 'master', link: '/' },
+              { text: 'v1.11', link: 'https://docs.px4.io/v1.11/en/' },
+              { text: 'v1.10', link: 'https://dev.qgroundcontrol.com/en/' },
+              { text: 'v1.9', link: 'https://docs.px4.io/v1.9.0/en/' },
+              { text: 'v1.8', link: 'https://docs.px4.io/v1.8.2/en/' },
+              
+            ]
+          },
+          
+        ],
+        sidebar: {
+          '/ko/': getSidebar.sidebar('ko')
+        }
+      },
+      
+      //Chinese
       '/zh/': {
         selectText: '选择语言',
-        label: '简体中文',
+        label: '中文 (Chinese)',
         editLinkText: '在 GitHub 上编辑此页',
         serviceWorker: {
           updatePopup: {
