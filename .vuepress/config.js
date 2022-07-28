6979ad0146f81326750ed33e0f18cef0583d605c
Hamish Willee <hamishwillee@gmail.com>
2022-07-28 13:44:00 +1000

    Add version switcher to main

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -174,18 +174,16 @@ module.exports = {
             ariaLabel: 'Versions Menu',
             items: [
               { text: 'main', link: 'https://docs.px4.io/main/en/' },
+              { text: 'v1.13', link: 'https://docs.px4.io/v1.13/en/' },
               { text: 'v1.12', link: 'https://docs.px4.io/v1.12/en/' },
               { text: 'v1.11', link: 'https://docs.px4.io/v1.11/en/' },
-              { text: 'v1.10', link: 'https://docs.px4.io/v1.10/en/' },
               {
                 text: 'DevGuide (Old/Merged)',
                 ariaLabel: 'Old Devguide',
                 items: [
                   { text: 'DevGuide v1.11', link: 'https://dev.px4.io/v1.11_noredirect/en/' },
-                  { text: 'DevGuide v1.10', link: 'https://dev.px4.io/v1.10_noredirect/en/' },
                 ]
               },
-              
             ]
           },
 
@@ -257,15 +255,14 @@ module.exports = {
             ariaLabel: 'Versions Menu',
             items: [
               { text: 'main', link: 'https://docs.px4.io/main/ko/' },
+              { text: 'v1.13', link: 'https://docs.px4.io/v1.12/ko/' },
               { text: 'v1.12', link: 'https://docs.px4.io/v1.12/ko/' },
               { text: 'v1.11', link: 'https://docs.px4.io/v1.11/en/' },
-              { text: 'v1.10', link: 'https://docs.px4.io/v1.10/en/' },
               {
                 text: 'DevGuide (Old/Merged)',
                 ariaLabel: 'Old Devguide',
                 items: [
                   { text: 'DevGuide v1.11', link: 'https://dev.px4.io/v1.11_noredirect/en/' },
-                  { text: 'DevGuide v1.10', link: 'https://dev.px4.io/v1.10_noredirect/en/' },
                 ]
               },
 
@@ -332,15 +329,14 @@ module.exports = {
             ariaLabel: 'Versions Menu',
             items: [
               { text: 'main', link: 'https://docs.px4.io/main/zh/' },
+              { text: 'v1.13', link: 'https://docs.px4.io/v1.13/zh/' },
               { text: 'v1.12', link: 'https://docs.px4.io/v1.12/zh/' },
               { text: 'v1.11', link: 'https://docs.px4.io/v1.11/zh/' },
-              { text: 'v1.10', link: 'https://docs.px4.io/v1.10/zh/' },
               {
                 text: 'DevGuide (Old/Merged)',
                 ariaLabel: 'Old Devguide',
                 items: [
                   { text: 'DevGuide v1.11', link: 'https://dev.px4.io/v1.11_noredirect/en/' },
-                  { text: 'DevGuide v1.10', link: 'https://dev.px4.io/v1.10_noredirect/en/' },
                 ]
               },
               
