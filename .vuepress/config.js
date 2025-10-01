2296a40e90e19e051a603bcee5fd0ce24ebcd0fa
Hamish Willee <hamishwillee@gmail.com>
2025-10-01 19:01:49 +1000

    [1.12] Add 1.16 ver and remove v1.11

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -233,11 +233,11 @@ module.exports = {
             ariaLabel: "Versions Menu",
             items: [
               { text: "main", link: "https://docs.px4.io/main/en/" },
+              { text: "v1.16", link: "https://docs.px4.io/v1.16/en/" },
               { text: "v1.15", link: "https://docs.px4.io/v1.15/en/" },
               { text: "v1.14", link: "https://docs.px4.io/v1.14/en/" },
               { text: "v1.13", link: "https://docs.px4.io/v1.13/en/" },
               { text: "v1.12", link: "https://docs.px4.io/v1.12/en/" },
-              { text: "v1.11", link: "https://docs.px4.io/v1.11/en/" },
             ],
           },
         ],
